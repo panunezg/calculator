158c69cf32f5255732081bec84e5bfb8a610319c
pablo <pablo@pablo-HP>
2015-01-30 00:32:16 -0500

    creacion clase java

---
+++ src/main/java/co/edu/bosque/swii/calculator/calculator/Calculador.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.bosque.swii.calculator.calculator;
+
+/**
+ *
+ * @author pablo
+ */
+public class Calculador {
+    
+    public double sumar(double numero1, double numero2){
+    return numero1 + numero2; 
+    } 
+     public double restar(double numero1, double numero2){
+    return numero1 - numero2; 
+    } 
+     public double multiplicar(double numero1, double numero2){
+    return numero1 * numero2; 
+    }
+
+    public double dividir(double numero1, double numero2){
+    return numero1 / numero2; 
+    }
+}
